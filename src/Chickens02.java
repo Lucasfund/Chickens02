5c33cb5b4be84ff30db1d1bd860f777397f2d299
ALUNO <ALUNO@LAB04-PC36>
2024-06-05 19:50:25 -0300

    Commit project chiken02

---
+++ src/Chickens02.java
@@ -0,0 +1,22 @@
+public class Chickens02 {
+    public static void main(String[] args) {
+//Abaixo o Valor por semana de ovos colhidos.      
+System.out.println("Segunda Feira 100");
+System.out.println("Terca Feira 121");
+System.out.println("Quarta Feira 117");
+
+System.out.println("Qual media diaria de ovos recolhidos?");
+double x = (100 +121 +117) / 3.0;
+System.out.println("O total sera " +x);
+
+System.out.println("Qual media mensal de ovos recolhidos?");
+Double a = 112.6667 * 3.0;
+System.out.println("O total sera " +a);
+
+System.out.println("Qual lucro mensal de ovos recolhidos?");
+Double b = 338000 * 0.18;
+System.out.println("O total sera " +b);
+
+    }
+    
+}
